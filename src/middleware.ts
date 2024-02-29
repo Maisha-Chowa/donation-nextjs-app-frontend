3a7ca889c24ff84d9e4ecfdf813f7c5a888c2955
Maisha-Chowa <maisha.chowa@gmail.com>
2024-02-29 19:07:15 +0600

    admin-dashboard-error-solve

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,14 +4,37 @@ import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
-  const { pathname } = request.nextUrl;
-  const role = token?.role;
-  console.log(role);
-  if (role === "admin" && pathname.startsWith("/admin")) {
-    return NextResponse.next();
+  if (token) {
+    const { pathname } = request.nextUrl;
+    const url = request.nextUrl.clone();
+    const role = token?.role;
+    if (role === "admin" && pathname.startsWith("/admin")) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(url.origin);
+    }
   }
-  return NextResponse.redirect(`${process.env.BASE_URL}`);
 }
 export const config = {
   matcher: "/admin/:page*",
 };
+
+// import { getToken } from "next-auth/jwt";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+
+// export async function middleware(request: NextRequest) {
+//   const token = await getToken({ req: request });
+//   const { pathname } = request.nextUrl;
+//   const role = token?.role;
+//   console.log("this si", role);
+//   if (role === "admin" && pathname.startsWith("/admin")) {
+//     return NextResponse.next();
+//   }
+//   return NextResponse.redirect(
+//     "https://donation-nextjs-app-frontend.vercel.app/"
+//   );
+// }
+// export const config = {
+//   matcher: "/admin/:page*",
+// };
