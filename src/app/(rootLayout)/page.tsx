d2085179b94d2d90675f6cac06715aeeded9cf0e
Maisha-Chowa <maisha.chowa@gmail.com>
2024-03-31 13:05:36 +0600

    Achievement-section

--- src/app/(rootLayout)/page.tsx
+++ src/app/(rootLayout)/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/ui/About";
+import Achievement from "@/components/ui/Achievement";
 import FeaturedDonation from "@/components/ui/FeaturedDonation";
 import HeroSection from "@/components/ui/HeroSection";
 import React from "react";
@@ -7,8 +8,9 @@ const page = () => {
   return (
     <div>
       <HeroSection />
-      <About />
+      <Achievement />
       <FeaturedDonation />
+      <About />
     </div>
   );
 };
